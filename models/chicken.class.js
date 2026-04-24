7ba1c98ce236d4d3e3beff8957417cd9a35ca3b7
Sahin Varol <sahin@varol-dev.com>
2026-04-24 14:08:07 +0200

    moving chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -12,6 +12,7 @@ class Chicken extends MovableObject {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 400 + Math.random() * 500;
     this.loadImages(this.IMAGES_WALKING);
+    this.speed = 0.3 + Math.random() * 0.25;
     this.animate();
   }
 
@@ -24,5 +25,7 @@ class Chicken extends MovableObject {
         this.currentImage = 0;
       }
     }, 200);
+
+    this.moveLeft();
   }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -6,6 +6,7 @@ class MovableObject {
   width = 100;
   imageCache = [];
   currentImage = 0;
+  speed = 0.15;
 
   loadImage(path) {
     this.img = new Image();
@@ -26,7 +27,8 @@ class MovableObject {
   }
 
   moveLeft() {
-    this.x -= 10;
-    console.log("moving left");
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
   }
 }
