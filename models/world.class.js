50365194980c5c9e763105db5fe0d47ff4611626
Sahin Varol <sahin@varol-dev.com>
2026-04-24 13:19:41 +0200

    first commit: finished core design and architecture

---
+++ models/world.class.js
@@ -0,0 +1,45 @@
+class World {
+  character = new Character();
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  ctx;
+  canvas;
+  clouds = [new Cloud(), new Cloud(), new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("../img/5_background/layers/air.png"),
+    new BackgroundObject("../img/5_background/layers/3_third_layer/1.png"),
+    new BackgroundObject("../img/5_background/layers/2_second_layer/1.png"),
+    new BackgroundObject("../img/5_background/layers/1_first_layer/1.png"),
+  ];
+
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
+    this.addObjectsToMap(this.clouds);
+    requestAnimationFrame(() => this.draw());
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((object) => {
+      this.addToMap(object);
+    });
+  }
+
+  addToMap(movableObject) {
+    this.ctx.drawImage(
+      movableObject.img,
+      movableObject.x,
+      movableObject.y,
+      movableObject.width,
+      movableObject.height,
+    );
+  }
+}
