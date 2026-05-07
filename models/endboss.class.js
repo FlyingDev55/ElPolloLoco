2365c060965b6f72eff54c88b7d18d7d0d616284
Sahin Varol <sahin@varol-dev.com>
2026-05-07 15:28:36 +0200

    created faster chicken for coins

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -55,6 +55,10 @@ class Endboss extends Chicken {
     if (Math.random() < 0.5) {
       chicken = new Chicken();
       chicken.y = 360;
+
+      if (Math.random() < 1 / 3) {
+        chicken.speed *= 5;
+      }
     } else {
       chicken = new MiniChicken();
       chicken.y = 385;
--- models/world.class.js
+++ models/world.class.js
@@ -166,6 +166,7 @@ class World {
 
     this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.level.backgroundObjects);
+    this.addObjectsToMap(this.level.clouds);
 
     this.ctx.translate(-this.camera_x, 0);
     this.addToMap(this.statusbar);
@@ -174,7 +175,6 @@ class World {
     }
     this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectsToMap(this.level.clouds);
     this.addObjectsToMap(this.level.enemies);
     this.addToMap(this.character);
     this.addObjectsToMap(this.throwableObjects);
